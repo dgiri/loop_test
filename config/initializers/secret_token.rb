0e0976bd11e9fe3eb926590c22192acb25162a57
Debashis Giri <dgiri@crri.co.in>
2011-10-07 15:49:14 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoopTest::Application.config.secret_token = '45acf0e9d4fc5b4695ad14e53c5b07e120007f315dc61d99f55c7e2b0ea5be0671cb4d22aa8bb4bb8d851bc3b3df24eba427233c380d2cc83eb5ffa2f4980a7b'
